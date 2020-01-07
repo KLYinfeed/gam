391a465106b44974bce5b00ce9ed3875ad36241d
yudakly <48110348+yudakly@users.noreply.github.com>
2020-01-07 11:45:47 +0700

    get order, get line item, get company, get companies

--- src/KLYinfeed/GAM/Traits/AdminTrait.php
+++ src/KLYinfeed/GAM/Traits/AdminTrait.php
@@ -7,6 +7,22 @@ trait AdminTrait
     **/
     public function get_advertisers($params=null)
     {
-        return $this->_get('get-advertisers');
+        return $this->_get('advertisers');
+    }
+    
+    /**
+     * GET INFO COMPANIES
+    **/
+    public function get_companies($params=null)
+    {
+        return $this->_get('companies');
+    }
+    
+    /**
+     * GET INFO COMPANIE
+    **/
+    public function get_company($params=null)
+    {
+        return $this->_get('company/' . $params['id']);
     }
 }
--- src/KLYinfeed/GAM/Traits/GeneralTrait.php
+++ src/KLYinfeed/GAM/Traits/GeneralTrait.php
@@ -9,4 +9,28 @@ trait GeneralTrait
     {
         return $this->_get('network');
     }
+    
+    /**
+     * GET INFO ORDERS
+    **/
+    public function get_orders($params=null)
+    {
+        return $this->_get('orders');
+    }
+    
+    /**
+     * GET INFO ORDER
+    **/
+    public function get_order($params=null)
+    {
+        return $this->_get('order/' . $params['id']);
+    }
+    
+    /**
+     * GET INFO LINE ITEM
+    **/
+    public function get_lineitem($params=null)
+    {
+        return $this->_get('lineitem/' . $params['id']);
+    }
 }
