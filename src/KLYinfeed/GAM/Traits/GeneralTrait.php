7f1ee2beae570846520bb4358e7112270544c6c7
yudakly <48110348+yudakly@users.noreply.github.com>
2020-04-27 16:40:11 +0700

    Update GeneralTrait.php

--- src/KLYinfeed/GAM/Traits/GeneralTrait.php
+++ src/KLYinfeed/GAM/Traits/GeneralTrait.php
@@ -19,11 +19,19 @@ trait GeneralTrait
     }
     
     /**
+     * GET CITIES
+    **/
+    public function get_cities()
+    {
+        return $this->_get('cities');
+    }
+
+    /**
      * GET INFO ORDER
     **/
     public function get_order($params=null)
     {
-        return $this->_get('order/' . $params['id']);
+        return $this->_get('order/' . $params['id']); 
     }
     
     /**
@@ -32,5 +40,13 @@ trait GeneralTrait
     public function get_lineitem($params=null)
     {
         return $this->_get('lineitem/' . $params['id']);
+    }
+	
+	 /**
+     * GET INFO CREATIVE
+    **/
+    public function get_creative($params=null)
+    {
+        return $this->_get('creative/' . $params['id']);
     }
 }
