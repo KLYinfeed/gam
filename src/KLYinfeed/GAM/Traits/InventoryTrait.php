b9c7bcb9eb0f9625332de591369a6b2906adb9e7
yudakly <48110348+yudakly@users.noreply.github.com>
2021-11-09 12:38:11 +0700

    Update InventoryTrait.php

--- src/KLYinfeed/GAM/Traits/InventoryTrait.php
+++ src/KLYinfeed/GAM/Traits/InventoryTrait.php
@@ -14,4 +14,14 @@ trait InventoryTrait
     {
         return $this->_get('placements');
     }
+
+    public function get_segment($params=null)
+    {
+        return $this->_get('segment', $params);
+    }
+
+    public function post_lineitem($params=null)
+    {
+        return $this->_post('lineitem', $params);
+    }
 }
